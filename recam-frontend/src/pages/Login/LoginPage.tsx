c99fac50042b4d7d4697a63ec7e2e0fad44e3bbf
HRY21 <70255962+Carrie124@users.noreply.github.com>
2026-05-29 21:28:01 +1000

    feat(RECAM-3): add registration page with form validation and reuseable InputFiled component

--- recam-frontend/src/pages/Login/LoginPage.tsx
+++ recam-frontend/src/pages/Login/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import InputField from '../../components/InputField'
 
 const LoginPage = () => {
     const [email, setEmail] = useState<string>('')
@@ -11,36 +12,31 @@ const LoginPage = () => {
         }else{
             console.log('email:', email)
             console.log('password:', password)
+            setError("")
         }
         
     }
     return(
         <div className="min-h-screen flex items-center justify-center bg-gray-50">
-            <div className="bg-white p-18 rounded-xl shadow-md w-full max-w-xl">
+            <div className="bg-white p-12 rounded-xl shadow-md w-full max-w-xl">
                 <h1 className="text-2xl font-bold text-gray-800 mb-6">
                     Welcome to House Showcase
                 </h1>
                 <p className="text-sm text-gray-500 mb-6">Please sign in to continue</p>
-                <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                    <input
-                        type="email"
-                        placeholder="Please enter your email"
-                        className="w-full border border-gray-300 rounded-lg px-4 py-2 text-sm"
-                        value={email}
-                        onChange={(e)=> setEmail(e.target.value)}
-                    />
-                </div>
-                <div className='mb-4'>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                    <input
-                        type="password"
-                        placeholder="Please enter your password"
-                        className="w-full border border-gray-300 rounded-lg px-4 py-2 text-sm"
-                        value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
-                    />
-                </div>
+                <InputField
+                label="Email"
+                type="email"
+                placeholder="Please enter your email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+                <InputField
+                label="Password"
+                type="password"
+                placeholder="Please enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
                 <button 
                     className="w-full bg-blue-400 mt-3 py-2 rounded-lg text-white"
                     onClick={handelLogin}>Login</button>
